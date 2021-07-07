ce4772cf70d536e0dd4aa2b4cebf2eb1e6ad6d02
Hassan-Leo <hassanahmed753@gmail.com>
2021-07-07 20:16:38 +0500

    New changes made test

--- test/newtest.js
+++ test/newtest.js
@@ -5,8 +5,8 @@ const chaihttp = require("chai-http");
 chai.should();
 chai.use(chaihttp);
 
-describe("Testing API",()=> {
-    it("Json placeholder APi todo test",(done)=> {
+describe("Testing API Testing by Faizan",()=> {
+    it("Json placeholder APi todo test by Faizan",(done)=> {
         chai.request("https://jsonplaceholder.typicode.com")
         .get("/todos/1")
         .end((err,resp)=> {
